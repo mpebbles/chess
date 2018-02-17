5bacb8d5826e86fe4ade56358cf098848b6426f5
Mitchell Pebbles <mitchell.jeffrey.pebbles@gmail.com>
2018-02-17 15:15:59 -0800

    Fixed issue with outputting computer's move.

--- computer_side.cpp
+++ computer_side.cpp
@@ -9,8 +9,8 @@ bool makeMove(Piece * pieces[16], Piece * board[8][8])
                 for(int b = 0; b < 8; ++b) {
                     if(pieces[i]->row != a and pieces[i]->col != b) {
                         if(pieces[i]->move(a, b, board)) {
-                            std::cout << "black>move " << pieces[i]->row << pieces[i]->col
-                            << " " << a << b << std::endl;
+                            std::cout << "black>move " << pieces[i]->col + 1 << pieces[i]->row + 1
+                            << " " << b + 1 << a + 1 << std::endl;
                             return true;
                         }
                     }
